f58da0501bfed25895e11be56e48781ad8366a30
FabriceT <fabrice.tsiava@artik-consulting.com>
2021-06-05 17:10:45 +0200

    FILL = set screen color black or white
    written in HACK machine language - assembly
    
    pass automatic test

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,121 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+// SCREEN = 16384
+
+// KBD = 24576
+(INIT)
+    @selectedColor
+    M=0     // selectedColor = white
+
+    @currentColor
+    M=0     // selectedColor = white
+
+    // SCREEN is 256 rows x 512 cols
+    // 512 cols = 32 x 16-bit registers
+
+    // 256 rows
+    // 32 registers / row
+    // 256 * 32 = 8192 registers
+    // from 0 to 8191
+    @8191
+    D=A
+    @lastScreenSlot 
+    M=D     // lastScreenSlot = 8191
+
+(RESET)
+    @FILL_SCREEN
+    0;JMP
+
+(START) // Infinite loop
+
+    // check state of keyboard
+    // if 0 set selectedColor to white = 0
+    // else set selectedColor to black = -1
+    @KBD
+    D=M
+    
+    @SET_WHITE
+    D;JEQ
+
+    @SET_BLACK
+    0;JMP
+
+(SET_WHITE)
+    @selectedColor
+    M=0
+
+    @CHECK_COLOR
+    0;JMP
+
+(SET_BLACK)
+    @selectedColor
+    M=-1
+
+    @CHECK_COLOR
+    0;JMP
+
+(CHECK_COLOR)
+    // compoare current and selected colors
+    // if same do nothing and loop
+    // else update currentColor and update screen
+    @selectedColor
+    D=M     // D = selectedColor
+
+    @currentColor
+    D=D-M   // D = selectedColor - currentColor
+
+    @START
+    D;JEQ   // if selectedColor - currentColor == 0 => selectedColor == currentColor 
+            // => no screen update goto START
+
+(UPDATE_CURRENT_COLOR)
+    // else currentColor = selectedColor and fill screen
+    @selectedColor
+    D=M     // D = selectedColor
+
+    @currentColor
+    M=D     // currentColor = selectedColor
+
+(FILL_SCREEN)
+    @i
+    M=0     // let i = 0
+
+    @SCREEN
+    D=A     // D = SCREEN
+
+    @screenPointer  
+    M=D     // let screenPointer = SCREEN
+
+(LOOP)
+    // load i
+    @i
+    D=M     // D = i
+
+    @lastScreenSlot
+    D=D-M   // D = i - lastScreenSlot 
+    
+    @START
+    D;JGT   // if i - lastScreenSlot > 0 => i > lastScreenSlot 
+            // => loop is finished goto START
+
+(FILL_SLOT)
+    @currentColor
+    D=M     // load currentColor
+
+    @screenPointer 
+    A=M     // go to address pointed by screenPointer
+    M=D     // fill pointed register with currentColor
+            // RAM[RAM[screenPointer]] = currentColor
+
+(INC_PTR)
+    @screenPointer
+    M=M+1   // screenPointer++
+
+(INC_I)
+    @i
+    M=M+1   // i++
+
+
+    @LOOP
+    0;JMP   // loop

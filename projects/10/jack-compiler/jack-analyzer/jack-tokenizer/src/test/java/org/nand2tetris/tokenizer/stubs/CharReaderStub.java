8e1e99e757cbfc21bf190f6a01ba32a2a5aa378f
FabriceT <fabrice.tsiava@artik-consulting.com>
2021-10-06 21:55:26 +0200

    jack tokenizer: stub char reader

---
+++ projects/10/jack-compiler/jack-analyzer/jack-tokenizer/src/test/java/org/nand2tetris/tokenizer/stubs/CharReaderStub.java
@@ -0,0 +1,33 @@
+package org.nand2tetris.tokenizer.stubs;
+
+
+import java.util.LinkedList;
+import java.util.Queue;
+import org.nand2tetris.tokenizer.CharReader;
+
+public class CharReaderStub implements CharReader {
+
+  Queue<Character> queue;
+
+  public CharReaderStub(String stream) {
+    queue = new LinkedList<>();
+    for (int i = 0; i < stream.length(); i++) {
+      queue.add(stream.charAt(i));
+    }
+  }
+
+  @Override
+  public void advance() {
+    queue.remove();
+  }
+
+  @Override
+  public char peekChar() {
+    return queue.element();
+  }
+
+  @Override
+  public boolean isEOF() {
+    return queue.isEmpty();
+  }
+}

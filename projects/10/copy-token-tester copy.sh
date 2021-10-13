a219c1776d450b555fa3a918febc33659bace22e
FabriceT <fabrice.tsiava@artik-consulting.com>
2021-10-13 22:46:45 +0200

    jack-tokenizer-tester: add shell scripts

---
+++ projects/10/copy-token-tester copy.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+cp jack-compiler/jack-analyzer/jack-tokenizer-tester/target/tokenizer-tester-uber.jar .
+# java -jar lib/jack-analyzer.jar "$@"
---
+++ projects/10/token-tester.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+java -jar tokenizer-tester-uber.jar "$@"

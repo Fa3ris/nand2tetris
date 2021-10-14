cd3cea3f32564f146b63b3be363754d5691b1fc9
FabriceT <fabrice.tsiava@artik-consulting.com>
2021-10-14 21:59:29 +0200

    add shelljack-tokenizer-tester: add shell scripts
    - Square successful

---
+++ projects/10/tokenizeSquare.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+paths=(./Square/Main.jack ./Square/Square.jack ./Square/SquareGame.jack)
+
+
+for element in "${paths[@]}"
+do
+    echo tokenize "$element"
+    ./token-tester.sh "$element"
+done
+
+./compareExpressionLessSquare.sh

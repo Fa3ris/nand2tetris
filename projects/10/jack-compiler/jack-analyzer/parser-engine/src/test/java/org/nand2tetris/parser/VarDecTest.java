03505840d617bd23ca2b58306d025745d2a197ac
FabriceT <fabrice.tsiava@artik-consulting.com>
2021-11-14 17:11:33 +0100

    jack-analyzer: refactor test parse var dec to use test utils methods

--- projects/10/jack-compiler/jack-analyzer/parser-engine/src/test/java/org/nand2tetris/parser/VarDecTest.java
+++ projects/10/jack-compiler/jack-analyzer/parser-engine/src/test/java/org/nand2tetris/parser/VarDecTest.java
@@ -1,34 +1,23 @@
 package org.nand2tetris.parser;
 
 import static org.nand2tetris.parser.test_utils.TestUtils.assertASTXML;
-import static org.nand2tetris.parser.utils.TagNames.varDec;
+import static org.nand2tetris.parser.test_utils.TestUtils.varDecTags;
+import static org.nand2tetris.parser.test_utils.TestUtils.varDecTokens;
 import static org.nand2tetris.parser.utils.XMLUtils.booleanTag;
 import static org.nand2tetris.parser.utils.XMLUtils.charTag;
-import static org.nand2tetris.parser.utils.XMLUtils.commaTag;
-import static org.nand2tetris.parser.utils.XMLUtils.concat;
-import static org.nand2tetris.parser.utils.XMLUtils.encloseInTag;
 import static org.nand2tetris.parser.utils.XMLUtils.identifierTag;
 import static org.nand2tetris.parser.utils.XMLUtils.intTag;
-import static org.nand2tetris.parser.utils.XMLUtils.semicolonTag;
-import static org.nand2tetris.parser.utils.XMLUtils.varTag;
 import static org.nand2tetris.tokenizer.Token.booleanToken;
 import static org.nand2tetris.tokenizer.Token.charToken;
-import static org.nand2tetris.tokenizer.Token.comma;
 import static org.nand2tetris.tokenizer.Token.identifierToken;
 import static org.nand2tetris.tokenizer.Token.intToken;
-import static org.nand2tetris.tokenizer.Token.semicolon;
-import static org.nand2tetris.tokenizer.Token.varToken;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import org.junit.Test;
-import org.nand2tetris.parser.test_utils.TestUtils;
-import org.nand2tetris.parser.utils.XMLUtils;
 import org.nand2tetris.tokenizer.Token;
 
 public class VarDecTest {
@@ -40,9 +29,9 @@ public class VarDecTest {
   @Test
   public void singleCharDec() throws Exception {
     String varName = "key";
-    List<Token> tokens = varDecTokens(charToken(), identifierToken(varName));
-    List<String> tags = varDecTags(charTag(), identifierTag(varName));
-    assertXML(tokens, tags);
+    List<Token> tokens = varDecTokens(charToken(), varName);
+    List<String> tags = varDecTags(charTag(), varName);
+    assertASTXML(tokens, tags);
   }
 
   /**
@@ -53,11 +42,10 @@ public class VarDecTest {
     String foo = "foo";
     String bar = "bar";
     List<Token> tokens = varDecTokens(charToken(),
-        Arrays.asList(identifierToken(foo), identifierToken(bar)
-    ));
+        Arrays.asList(foo, bar));
     List<String> tags = varDecTags(charTag(),
-        Arrays.asList(identifierTag(foo), identifierTag(bar)));
-    assertXML(tokens, tags);
+        Arrays.asList(foo, bar));
+    assertASTXML(tokens, tags);
 
   }
   /**
@@ -66,9 +54,9 @@ public class VarDecTest {
   @Test
   public void intVarDec() throws Exception {
     String n = "n";
-    List<Token> tokens = varDecTokens(intToken(), identifierToken(n));
-    List<String> tags = varDecTags(intTag(), identifierTag(n));
-    assertXML(tokens, tags);
+    List<Token> tokens = varDecTokens(intToken(), n);
+    List<String> tags = varDecTags(intTag(), n);
+    assertASTXML(tokens, tags);
   }
 
   /**
@@ -77,9 +65,9 @@ public class VarDecTest {
   @Test
   public void booleanVarDec() throws Exception {
     String id = "idKO";
-    List<Token> tokens = varDecTokens(booleanToken(), identifierToken(id));
-    List<String> tags = varDecTags(booleanTag(), identifierTag(id));
-    assertXML(tokens, tags);
+    List<Token> tokens = varDecTokens(booleanToken(), id);
+    List<String> tags = varDecTags(booleanTag(), id);
+    assertASTXML(tokens, tags);
   }
 
   /**
@@ -89,9 +77,9 @@ public class VarDecTest {
   public void classVarDec() throws Exception {
     String type = "MyClass";
     String id = "test1";
-    List<Token> tokens = varDecTokens(identifierToken(type), identifierToken(id));
-    List<String> tags = varDecTags(identifierTag(type), identifierTag(id));
-    assertXML(tokens, tags);
+    List<Token> tokens = varDecTokens(identifierToken(type), id);
+    List<String> tags = varDecTags(identifierTag(type), id);
+    assertASTXML(tokens, tags);
   }
 
   /**
@@ -105,54 +93,11 @@ public class VarDecTest {
         .mapToObj(Objects::toString)
         .collect(Collectors.toList());
 
-    List<Token> identifiers = idNames.stream()
-        .map(Token::identifierToken)
-        .collect(Collectors.toList());
+    List<Token> tokens = varDecTokens(charToken(), idNames);
 
-    List<Token> tokens = varDecTokens(charToken(), identifiers);
+    List<String> tags = varDecTags(charTag(), idNames);
 
-    List<String> identifierTags = idNames.stream()
-        .map(XMLUtils::identifierTag)
-        .collect(Collectors.toList());
+    assertASTXML(tokens, tags);
 
-    List<String> tags = varDecTags(charTag(), identifierTags);
-
-    assertXML(tokens, tags);
-
-  }
-
-  private List<Token> varDecTokens(Token type, Token identifier) {
-    return varDecTokens(type, Collections.singletonList(identifier));
-  }
-
-  private List<Token> varDecTokens(Token type, List<Token> identifiers) {
-    List<Token> tokens = new ArrayList<>();
-    tokens.add(varToken());
-    tokens.add(type);
-    List<Token> joinedIdentifiers = TestUtils.joinTokens(identifiers, comma());
-    tokens.addAll(joinedIdentifiers);
-    tokens.add(semicolon());
-    return tokens;
-  }
-
-
-  private List<String> varDecTags(String type, String identifier) {
-    return varDecTags(type, Collections.singletonList(identifier));
-  }
-
-  private List<String> varDecTags(String type, List<String> identifiers) {
-    List<String> tags = new ArrayList<>();
-    tags.add(varTag());
-    tags.add(type);
-    List<String> joinedIdentifiers = TestUtils.joinTags(identifiers, commaTag());
-    tags.addAll(joinedIdentifiers);
-    tags.add(semicolonTag());
-    return tags;
-  }
-
-  private void assertXML(List<Token> tokens, List<String> tags) {
-    List<String> enclosed = encloseInTag(varDec, tags);
-    String expectedXML = concat(enclosed);
-    assertASTXML(tokens, expectedXML);
   }
 }

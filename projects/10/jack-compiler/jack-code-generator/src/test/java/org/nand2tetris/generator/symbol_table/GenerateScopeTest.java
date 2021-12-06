004441f226918e55b11d5a1e79025e4dfa24a503
FabriceT <fabrice.tsiava@artik-consulting.com>
2021-12-06 23:38:05 +0100

    jack-code-generator: ignore line comment in test file - refactor with regex

--- projects/10/jack-compiler/jack-code-generator/src/test/java/org/nand2tetris/generator/symbol_table/GenerateScopeTest.java
+++ projects/10/jack-compiler/jack-code-generator/src/test/java/org/nand2tetris/generator/symbol_table/GenerateScopeTest.java
@@ -7,6 +7,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.function.Function;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import org.junit.Assert;
 import org.junit.Test;
@@ -53,16 +55,18 @@ public class GenerateScopeTest {
   }
 
   private Function<List<String>, List<String>> stripLineComments() {
-    return list -> list.stream().map(stripLineComment()).collect(Collectors.toList());
+    return list -> list.stream().map(stripLineComment()).filter(s -> !s.isEmpty()).collect(Collectors.toList());
   }
 
   private Function<String, String> stripLineComment() {
     return s -> {
-      int commentStart = s.indexOf("//");
-      if (commentStart > -1) {
-        s = s.substring(0, commentStart).trim();
+      Matcher matcher = beforeDoubleSlash.matcher(s);
+      if (matcher.find()) {
+        s = matcher.group(1).trim();
       }
       return s;
     };
   }
+
+  private final Pattern beforeDoubleSlash = Pattern.compile("(.*?)//");
 }

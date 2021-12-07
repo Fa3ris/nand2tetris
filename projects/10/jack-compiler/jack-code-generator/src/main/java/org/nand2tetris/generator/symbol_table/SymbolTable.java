a323fd3c835193d9f15973c5fd4b1299eb741479
FabriceT <fabrice.tsiava@artik-consulting.com>
2021-12-07 21:48:51 +0100

    jack-code-generator: refactor symbol table
    - change count method

--- projects/10/jack-compiler/jack-code-generator/src/main/java/org/nand2tetris/generator/symbol_table/SymbolTable.java
+++ projects/10/jack-compiler/jack-code-generator/src/main/java/org/nand2tetris/generator/symbol_table/SymbolTable.java
@@ -10,11 +10,11 @@ public class SymbolTable {
   private final Map<String, TableEntry> classTable = new HashMap<>();
   private final Map<String, TableEntry> subroutineTable = new HashMap<>();
 
-  private int fieldCount = 0;
-  private int staticCount = 0;
+  private int fieldCount = -1;
+  private int staticCount = -1;
 
-  private int localCount = 0;
-  private int argumentCount = 0;
+  private int localCount = -1;
+  private int argumentCount = -1;
 
   public void define(String name, Type type, Scope scope) {
     if (scope == Scope.LOCAL || scope == Scope.ARGUMENT) {
@@ -23,9 +23,9 @@ public class SymbolTable {
     }
     int index;
     if (scope == Scope.STATIC) {
-      index = staticCount++;
+      index = ++staticCount;
     } else {
-      index = fieldCount++;
+      index = ++fieldCount;
     }
     TableEntry newEntry = new TableEntry(name, type, scope, index);
     classTable.put(name, newEntry);
@@ -35,9 +35,9 @@ public class SymbolTable {
   private void addSubroutineTable(String name, Type type, Scope scope) {
     int index;
     if (scope == Scope.LOCAL) {
-      index = localCount++;
+      index = ++localCount;
     } else {
-      index = argumentCount++;
+      index = ++argumentCount;
     }
     TableEntry newEntry = new TableEntry(name, type, scope, index);
     subroutineTable.put(name, newEntry);
@@ -51,7 +51,11 @@ public class SymbolTable {
 
   public void resetSubroutine() {
     subroutineTable.clear();
-    localCount = 0;
-    argumentCount = 0;
+    localCount = -1;
+    argumentCount = -1;
+  }
+
+  public int getLocalCount() {
+    return localCount + 1;
   }
 }

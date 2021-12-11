893035652f0cdf370ed9e13c245bde717bed695c
FabriceT <fabrice.tsiava@artik-consulting.com>
2021-12-11 16:12:24 +0100

    jack-code-generator: test call constructor

---
+++ projects/10/jack-compiler/jack-code-generator/src/test/java/org/nand2tetris/generator/Constructor/ConstructorTest.java
@@ -0,0 +1,19 @@
+package org.nand2tetris.generator.Constructor;
+
+import static org.nand2tetris.generator.test_utils.VMAssert.assertCorrectVm;
+
+import java.io.File;
+import java.nio.file.Path;
+import org.junit.Test;
+
+public class ConstructorTest {
+
+
+  @Test
+  public void callConstructor() throws Exception {
+    String baseName = "CallConstructor";
+    Path inputPath = new File(getClass().getResource(baseName + ".jack").getFile()).toPath();
+    assertCorrectVm(inputPath);
+  }
+
+}
